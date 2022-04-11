8667199411de1edb0887209a1a790b8f1b4c868e
Joshua Oyewole  (Orisfina) <joshpp2013@gmail.com>
2022-04-11 22:39:36 +0100

    Rephrasing the slideshow heading

--- components/SlideShow.js
+++ components/SlideShow.js
@@ -7,7 +7,7 @@ export default function SlideShow() {
     <div className={styles.slideshow}>
       {/* <h1 className={styles.title}>YouthfulBusiness Blog</h1> */}
         <h2 className={styles.description}>
-        Beginnerswebguide can help you start a career in web development irrespective of your current level through simplified and easy to understand contents to get you started.
+        Beginnerswebguide can help you start a career in web development irrespective of your current level through simplified and easy to understand contents.
         </h2>
     </div>
   )
