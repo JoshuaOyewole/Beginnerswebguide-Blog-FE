ad421e56bcee0f4c3942481f991f2935587d4a78
Joshua Oyewole  (Orisfina) <joshpp2013@gmail.com>
2022-04-13 13:30:13 +0100

    Resolving Module not found error

--- components/SlideShow.js
+++ components/SlideShow.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import styles from '../styles/SlideShow.module.css'
+import Styles from '../styles/SlideShow.module.css'
 
 export default function SlideShow() {
 
   return (
-    <div className={styles.slideshow}>
-      {/* <h1 className={styles.title}>YouthfulBusiness Blog</h1> */}
-        <h2 className={styles.description}>
+    <div className={Styles.slideshow}>
+        <h2 className={Styles.description}>
         Beginnerswebguide can help you start a career in web development irrespective of your current level through simplified and easy to understand contents.
         </h2>
     </div>
