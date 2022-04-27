b199a96ed4ee32ec1b804e87a40642fe906dca12
Joshua Oyewole  (Orisfina) <joshpp2013@gmail.com>
2022-04-27 19:42:09 +0100

    resized the slideshow image

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
-const dev = process.env.NODE_ENV !== 'production';
-
-export const server = dev ? 'http://localhost:1337' : 'https://beginnerswebguide.herokuapp.com';
+module.exports = {
+    env: {
+        URL_API: 'localhost:1337',
+    },
+  }
